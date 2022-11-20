0bf4b2fd21b5be2fdcb33450491bf4e0631146c0
LeonWu813 <leonwuya@gamil.com>
2022-11-20 10:59:16 +0800

    name js

--- js/drag.js
+++ js/drag.js
@@ -1,22 +1,30 @@
-// dailysection 
-let startValue=0;
-const listCard0={
-    ele:document.querySelectorAll('.list-todo .card')[0],
-    hour:2,
+// Fill User Name
+function fillUserName() {
+  var user_name = document.getElementById("user_name").value;
+  document.getElementById("fill-user-name").innerText = document.getElementById(
+    "fill-user-name-last"
+  ).innerText = user_name;
 }
-const listCard1={
-    ele:document.querySelectorAll('.list-todo .card')[1],
-    hour:4,
-}
-const listCard2={
-    ele:document.querySelectorAll('.list-todo .card')[2],
-    hour:4,
-}
-const listCard3={
-    ele:document.querySelectorAll('.list-todo .card')[3],
-    hour:2,
-}
+
+// dailysection
+let startValue = 0;
+const listCard0 = {
+  ele: document.querySelectorAll(".list-todo .card")[0],
+  hour: 2,
+};
+const listCard1 = {
+  ele: document.querySelectorAll(".list-todo .card")[1],
+  hour: 4,
+};
+const listCard2 = {
+  ele: document.querySelectorAll(".list-todo .card")[2],
+  hour: 4,
+};
+const listCard3 = {
+  ele: document.querySelectorAll(".list-todo .card")[3],
+  hour: 2,
+};
 //if else hour =8
 
-// reset btn 
-const resetDaily=document.getElementById('daily-reset');
+// reset btn
+const resetDaily = document.getElementById("daily-reset");
