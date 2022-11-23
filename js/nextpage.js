6c0ef765f81fe9db9ea1bf2d59fdb26ef79691a9
XINYU714 <edc2312321@gmail.com>
2022-11-23 22:10:35 +0800

    transLayout-html

--- js/nextpage.js
+++ js/nextpage.js
@@ -1,4 +1,8 @@
 const allSection=document.querySelectorAll('.container >section');
-const preBtn=document.querySelector('.arrow-wrap .pre');
-const nextBtn=document.querySelector('.arrow-wrap .next');
+const preBtn=document.querySelectorAll('.arrow-wrap .pre');
+const nextBtn=document.querySelectorAll('.arrow-wrap .next');
+const againBtn=document.getElementById('again');
 let currentIndex=0;
+againBtn.onclick=()=>{
+    window.location.reload();
+}
